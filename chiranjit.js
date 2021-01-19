f361a96979ce25f54588ae9f9fe01235cf7f263e
Amarta Dey <34670651+amartadey@users.noreply.github.com>
2021-01-19 19:10:50 +0530

    Add files via upload

--- chiranjit.js
+++ chiranjit.js
@@ -1,34 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <title>Budget Tracker</title>
-    <link
-      rel="stylesheet"
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
-    />
-    <link rel="stylesheet" href="css/style.css" />
-  </head>
 
-  <body>
-    <i class="fab fa-facebook-f"></i>
+<head>
+    <meta charset="UTF-8">
+    <title>Budget Tracker</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
     <main>
-      <div class="menu-bar">
-        <ul>
-          <li>
-            <a href="#"
-              ><img
-                src="https://img.icons8.com/material/24/000000/four-squares.png"
-            /></a>
-          </li>
-          <li>
-            <a href="#"><i class="fab fa-facebook-messenger"></i></a>
-          </li>
-          <li><a href="#"></a></li>
-          <li><a href="#"></a></li>
-        </ul>
-      </div>
-      <div class="app-window"></div>
+        <h1>Budget Tracker</h1>
     </main>
-  </body>
+</body>
+
 </html>
