0aaca2479e1b6684b5b35d445804deb4391219a1
Amarta Dey <34670651+amartadey@users.noreply.github.com>
2021-01-22 17:15:23 +0530

    Added Flatpickr DateTime Module

--- js/app.js
+++ js/app.js
@@ -2,7 +2,10 @@ const calDateButtons = document.querySelector("#cal-date-buttons");
 const body = document.body;
 const main = document.querySelector("main");
 const userprofileBtn = document.querySelector("#user-profile-btn");
+const date = new Date();
+localStorage.setItem("trDate", date);
 let userName = localStorage.getItem("userName");
+let userEmail = localStorage.getItem("userEmail");
 sessionStorage.removeItem("profileBlob");
 
 generateFirstLine();
@@ -47,32 +50,42 @@ userprofileBtn.addEventListener("click", (e) => {
   });
   userProfile.querySelector("button.done").addEventListener("click", () => {
     userName = userProfile.querySelector("h2").innerText;
+    userEmail = userProfile.querySelector("p").innerText;
     localStorage.setItem("userName", userName);
+    localStorage.setItem("userEmail", userEmail);
     closeUI(userProfile);
     generateFirstLine();
   });
   userProfile.querySelector("h2").addEventListener("click", (e) => {
     e.target.setAttribute("contenteditable", "true");
+    e.target.focus();
+  });
+  userProfile.querySelector("p").addEventListener("click", (e) => {
+    e.target.setAttribute("contenteditable", "true");
+    e.target.focus();
   });
 });
 
 calDateButtons.querySelectorAll("a").forEach((anc, idx) => {
   anc.addEventListener("click", () => {
+    // if (idx == 0) {
+    //   const calenderUi = document.createElement("div");
+    //   calenderUi.className = "calendar-ui";
+    //   calenderUi.innerHTML = makeCalenderUi();
+
+    //   const calenderUiclose = calenderUi.querySelector(".calendar-close");
+
+    //   calenderUiclose.addEventListener("click", () => {
+    //     closeUI(calenderUi);
+    //   });
+    //   main.appendChild(calenderUi);
+    //   calenderFunctionality();
+    // }
     if (idx == 0) {
-      const calenderUi = document.createElement("div");
-      calenderUi.className = "calendar-ui";
-      calenderUi.innerHTML = makeCalenderUi();
-
-      const calenderUiclose = calenderUi.querySelector(".calendar-close");
-
-      calenderUiclose.addEventListener("click", () => {
-        closeUI(calenderUi);
-      });
-      main.appendChild(calenderUi);
-    } else if (idx == 1) {
       const addTransactionSection = document.createElement("div");
       addTransactionSection.className = "add-transaction-section";
       addTransactionSection.innerHTML = makeTransactionSection();
+
       const addTransactionClose = addTransactionSection.querySelector(
         ".close-button"
       );
@@ -80,8 +93,19 @@ calDateButtons.querySelectorAll("a").forEach((anc, idx) => {
         closeUI(addTransactionSection);
       });
       main.appendChild(addTransactionSection);
+      flatpickr("#datetime", {
+        enableTime: true,
+        dateFormat: "J M, Y G:i K",
+        onChange: showDate,
+      });
+      function showDate(e) {
+        const dateTimeTitle = addTransactionSection.querySelector("h2");
+
+        console.log(e[0]);
+        console.log(localStorage.getItem("trDate"));
+      }
+      body.className = "defuse";
     }
-    body.className = "defuse";
   });
 });
 
@@ -98,7 +122,15 @@ function makeTransactionSection() {
         </button>
         <h4>transaction on</h4>
         <h2>20th Jan, 2020 @ 01:16PM</h2>
-
+        <input
+        class="flatpickr flatpickr-input active"
+        id="datetime"
+        type="text"
+        placeholder="Select Date.."
+        data-id="datetime"
+        readonly="readonly"
+      />
+        
         <div class="form-block">
           <label>transaction name </label>
           <input type="text" class="tr-input" />
@@ -124,104 +156,115 @@ function makeTransactionSection() {
     `;
 }
 
-function makeCalenderUi() {
-  return `
-      <div class="content-block">
-        <div class="month-year-panel">
-          <div class="month">
-            <select name="months" id="">
-              <option value="january">January</option>
-              <option value="february">February</option>
-              <option value="march">March</option>
-              <option value="april">April</option>
-              <option value="may">May</option>
-              <option value="june">June</option>
-              <option value="july">July</option>
-              <option value="august">August</option>
-              <option value="september">September</option>
-              <option value="october">October</option>
-              <option value="november">November</option>
-              <option value="december">December</option>
-            </select>
-          </div>
-          <!-- .month -->
-          <div class="year">
-            <select name="" id="">
-              <option value="">2020</option>
-              <option value="" selected>2021</option>
-              <option value="">2022</option>
-              <option value="">2023</option>
-              <option value="">2024</option>
-              <option value="">2025</option>
-              <option value="">2026</option>
-            </select>
-          </div>
-        </div>
-        <!-- .month-year-panel -->
-        <div class="day-date-panel">
-          <div class="day">
-            <ul>
-              <li>Su</li>
-              <li>Mo</li>
-              <li>Tu</li>
-              <li>We</li>
-              <li>Th</li>
-              <li>Fr</li>
-              <li>Sa</li>
-            </ul>
-          </div>
-          <div class="date">
-            <ul>
-              <li>01</li>
-              <li>02</li>
-              <li>03</li>
-              <li>04</li>
-              <li>05</li>
-              <li>06</li>
-              <li>07</li>
-              <li>08</li>
-              <li>09</li>
-              <li>10</li>
-              <li>11</li>
-              <li>12</li>
-              <li>13</li>
-              <li>14</li>
-              <li>15</li>
-              <li>16</li>
-              <li>17</li>
-              <li>18</li>
-              <li>19</li>
-              <li>20</li>
-              <li class="current">21</li>
-              <li>22</li>
-              <li>23</li>
-              <li>24</li>
-              <li>25</li>
-              <li>26</li>
-              <li>27</li>
-              <li>28</li>
-              <li>29</li>
-              <li>30</li>
-              <li>31</li>
-            </ul>
-          </div>
-          <!-- .date -->
-          <div class="time-panel">
-            <input
-              type="text"
-              placeholder="HR-MM AM/PM"
-              class="form-control"
-              onfocus="this.type='time'"
-              onblur="this.type='text'"
-            />
-          </div>
-        </div>
-      </div>
-      <button class="calendar-close">
-        <i class="fas fa-times-circle"></i>
-      </button>
-    `;
-}
+// function makeCalenderUi() {
+//   const date = new Date();
+//   let hour = date.getHours() <= 12 ? date.getHours() : date.getHours() - 12;
+//   hour = hour < 10 ? "0" + hour : hour;
+//   let min = date.getMinutes();
+//   min = min < 10 ? "0" + min : min;
+//   const ampm = date.getHours() < 12 ? "AM" : "PM";
+//   return `
+//       <div class="content-block">
+//         <div class="month-year-panel">
+//           <div class="month">
+//             <select name="months" id="">
+//               <option value="january">January</option>
+//               <option value="february">February</option>
+//               <option value="march">March</option>
+//               <option value="april">April</option>
+//               <option value="may">May</option>
+//               <option value="june">June</option>
+//               <option value="july">July</option>
+//               <option value="august">August</option>
+//               <option value="september">September</option>
+//               <option value="october">October</option>
+//               <option value="november">November</option>
+//               <option value="december">December</option>
+//             </select>
+//           </div>
+//           <!-- .month -->
+//           <div class="year">
+//             <select name="" id="">
+//               <option value="">2020</option>
+//               <option value="" selected>2021</option>
+//               <option value="">2022</option>
+//               <option value="">2023</option>
+//               <option value="">2024</option>
+//               <option value="">2025</option>
+//               <option value="">2026</option>
+//             </select>
+//           </div>
+//         </div>
+//         <!-- .month-year-panel -->
+//         <div class="day-date-panel">
+//           <div class="day">
+//             <ul>
+//               <li>Su</li>
+//               <li>Mo</li>
+//               <li>Tu</li>
+//               <li>We</li>
+//               <li>Th</li>
+//               <li>Fr</li>
+//               <li>Sa</li>
+//             </ul>
+//           </div>
+//           <div class="date">
+//             <ul>
+//               <li>01</li>
+//               <li>02</li>
+//               <li>03</li>
+//               <li>04</li>
+//               <li>05</li>
+//               <li>06</li>
+//               <li>07</li>
+//               <li>08</li>
+//               <li>09</li>
+//               <li>10</li>
+//               <li>11</li>
+//               <li>12</li>
+//               <li>13</li>
+//               <li>14</li>
+//               <li>15</li>
+//               <li>16</li>
+//               <li>17</li>
+//               <li>18</li>
+//               <li>19</li>
+//               <li>20</li>
+//               <li class="current">21</li>
+//               <li>22</li>
+//               <li>23</li>
+//               <li>24</li>
+//               <li>25</li>
+//               <li>26</li>
+//               <li>27</li>
+//               <li>28</li>
+//               <li>29</li>
+//               <li>30</li>
+//               <li>31</li>
+//             </ul>
+//           </div>
+//           <!-- .date -->
+//           <div class="time-panel" style="position:relative">
+//             <input
+//               type="time"
+//               placeholder=""
+//               class="form-control"
+//               style="position:absolute; top:12; left:0; opacity:0; z-index:9; transform:rotate(180deg)"
+//             />
+//             <input
+//               type="text"
+//               placeholder="${hour + ":" + min + " " + ampm}"
+//               class="form-control"
+
+//             />
+//           </div>
+//         </div>
+//       </div>
+//       <button class="calendar-close">
+//         <i class="fas fa-times-circle"></i>
+//       </button>
+//     `;
+// }
 
 function makeUserProfile() {
   return `
@@ -238,7 +281,7 @@ function makeUserProfile() {
 
       <div class="user-details">
         <h2>${userName ? userName : "Random User"}</h2>
-        <p>example@gmail.com</p>
+        <p>${userEmail ? userEmail : "example@gmail.com"}</p>
         <button class="done">Done</button>
       </div>
     `;
